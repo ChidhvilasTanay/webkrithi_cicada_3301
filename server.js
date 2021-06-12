118e34e1127f8bc7f89451a4011ffd440823ee5a
ChidhvilasTanay <73900317+ChidhvilasTanay@users.noreply.github.com>
2021-06-12 15:54:51 +0530

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express();
-app.use('/', require('./data'));
-app.listen(8080, (req, res) => {
-    console.log('server at http://localhost:8080');
-});
